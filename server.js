f75da5ed65bac74d599ca47f3da3d68ae6b8fc88
Chowdhurian <manil.chowdhury@gmail.com>
2016-08-11 21:17:36 -0700

    created item model, need to create item get request

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
+var Item = require('./app/models/item');
+
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false}));
